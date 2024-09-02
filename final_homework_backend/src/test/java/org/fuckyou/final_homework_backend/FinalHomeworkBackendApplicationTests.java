c4480fb8da0038083105b5f52c3cbbfc4b724c66
Tom_Brown <homo11451445@outlook.com>
2024-09-02 16:22:43 +0800

    项目初始化和基础配置
    创建了初始的项目结构和配置文件，包括.gitignore、application.properties、pom.xml等。配置了基础的Maven构建脚本和IntelliJ IDEA、VS Code等编辑器的忽略规则。设定了Spring Boot应用的基本属性，添加了必要的依赖项，并进行了基础的包结构和启动类的创建。

---
+++ final_homework_backend/src/test/java/org/fuckyou/final_homework_backend/FinalHomeworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fuckyou.final_homework_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalHomeworkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
